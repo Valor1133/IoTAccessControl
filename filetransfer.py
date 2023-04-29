187271993b5a72698e3dbf2e9de9300bbfea26a0
Valor1133 <120724604+Valor1133@users.noreply.github.com>
2023-04-29 11:19:01 +0530

    final tweaks

---
+++ filetransfer.py
@@ -0,0 +1,36 @@
+import paramiko
+from cryptography.fernet import Fernet
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname='192.168.85.95', username='valor13', password='123')
+
+answer = input("Do you want to transfer files? Enter yes or no: ")
+ans2 = input("What file you want to transfer? ")
+
+# Encryption
+# opening the key
+with open('filekey.txt', 'rb') as filekey:
+	key = filekey.read()
+
+# using the generated key
+fernet = Fernet(key)
+
+# opening the original file to encrypt
+with open(ans2, 'rb') as file:
+	original = file.read()
+	
+# encrypting the file
+encrypted = fernet.encrypt(original)
+
+# opening the file in write mode and
+# writing the encrypted data
+print('Encrypted file will be sent over as' + encrypted)
+
+sftp = client.open_sftp()
+local_path = 'D:/modiji.jpeg'
+remote_path = '\Download\modiji.jpeg'
+sftp.put(local_path, remote_path)
+sftp.close()
+
+client.close()
